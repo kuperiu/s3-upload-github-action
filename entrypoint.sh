5da2701ef030c4a307c6e137a49689e4c0ededc2
Lior <lior@l01015153.lan>
2020-10-07 10:24:29 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,7 +23,7 @@ set -e
 # if [ -z "$AWS_REGION"]; then
 #   AWS_REGION="us-east-1"
 # fi
-
+echo ${GIT_SHA:0:10}.zip
 
 aws s3 cp ${FILE} s3://${S3_BUCKET}/${S3_KEY}/${GIT_SHA:0:10}.zip --region ${AWS_REGION} $*
 
