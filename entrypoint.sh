f9e1a404181eb6ba91ab2927346ce952f44705b5
Lior <lior@l01015153.lan>
2020-10-07 15:40:28 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ cat /tmp/test
 env
 echo ${AWS_IAM_TOKEN} > /var/run/secrets/eks.amazonaws.com/serviceaccount/token
 cat /var/run/secrets/eks.amazonaws.com/serviceaccount/token
-# aws s3 cp ${FILE} s3://${S3_BUCKET}/${GITHUB_REPOSITORY}/${GITHUB_SHA:0:10}.zip --region ${AWS_REGION} $*
-# exit $?
+aws s3 cp ${FILE} s3://${S3_BUCKET}/${GITHUB_REPOSITORY}/${GITHUB_SHA:0:10}.zip --region ${AWS_REGION} $*
+exit $?
 
-while true; do
- echo ""
-done
+# while true; do
+#  echo ""
+# done
