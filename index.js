faa579973526c0dfe1948b73b05f5ad499a0c546
Lior <lior@l01015153.lan>
2020-10-07 12:49:54 +0200

    test

---
+++ index.js
@@ -0,0 +1,11 @@
+const { exec } = require('child_process');
+exec('aws sts get-caller-identity', (err, stdout, stderr) => {
+  if (err) {
+    //some err occurred
+    console.error(err)
+  } else {
+   // the *entire* stdout and stderr (buffered)
+   console.log(`stdout: ${stdout}`);
+   console.log(`stderr: ${stderr}`);
+  }
+});
