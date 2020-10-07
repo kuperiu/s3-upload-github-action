cb91f214b497ead27add23cb0dae9ed84ebdb635
Lior <lior@l01015153.lan>
2020-10-07 12:32:44 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 GITHUB_REPOSITORY=$(basename $GITHUB_REPOSITORY)
-
+aws s3 ls
 aws s3 cp ${FILE} s3://${S3_BUCKET}/${GITHUB_REPOSITORY}/${GITHUB_SHA:0:10}.zip --region ${AWS_REGION} $*
 echo $?
