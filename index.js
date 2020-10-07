45ed2e28db1819fcebca87107b91a95185d1b741
Lior <lior@l01015153.lan>
2020-10-07 13:08:43 +0200

    test

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const AWS = require('aws-sdk');
 
 const s3 = new AWS.S3({});
 
-const fileName = 'contacts.csv';
+const fileName = process.env.FILE;
 
 const uploadFile = () => {
   fs.readFile(fileName, (err, data) => {
      if (err) throw err;
      const params = {
-         Bucket: 'process.env.S3_BUCKET', // pass your bucket name
-         Key: 'process.env.GITHUB_SHA', // file will be saved as testBucket/contacts.csv
+         Bucket: process.env.S3_BUCKET, // pass your bucket name
+         Key: process.env.GITHUB_SHA, // file will be saved as testBucket/contacts.csv
          Body: JSON.stringify(data, null, 2)
      };
      s3.upload(params, function(s3Err, data) {
